12515c5c9620f0cdd9194ef47ad33477bd7ceab4
Bernard-code <bernard.kacprzak93@gmail.com>
2025-06-18 01:01:28 +0200

    cors update

--- src/main/java/com/bernard/interview/config/DevCorsConfiguration.java
+++ src/main/java/com/bernard/interview/config/DevCorsConfiguration.java
@@ -1,8 +1,8 @@
 package com.bernard.interview.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,12 +10,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class DevCorsConfiguration implements WebMvcConfigurer {
 
+    @Value("${ALLOWED_ORIGINS:http://localhost:4200}")
+    private String allowedOrigins;
+
     @Bean
     public DevCorsConfiguration corsConfigurer() {
         return new DevCorsConfiguration() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**") // allow all endpoints
+                registry.addMapping("/**")
                         .allowedOrigins("http://localhost:4200") // Angular dev server
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
